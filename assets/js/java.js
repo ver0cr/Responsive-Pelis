adc7c6d4586ff11c0604bf618ce9b164a1c1c545
Micaela <94004537+micaela24c@users.noreply.github.com>
2021-12-22 10:13:04 -0500

    u

--- assets/js/java.js
+++ assets/js/java.js
@@ -2,16 +2,34 @@ function cargarPeliculas() {
   $.ajax({
     type: 'get',
     dataType: 'json',
-    url: `https://api.themoviedb.org/3/trending/all/day?api_key=f9d454a7908f5a64ae231240c823e0e4&language=es`,
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=c41bd0194b74d8255c364fa70f725d1a&language=es`,
     async: 'true',
-    success: function(data) {renderPelicula(data)},
+    success: function (data) { renderPelicula(data) },
+  });
+}
+//peliculas al azar de un género en específico: https://api.themoviedb.org/3/discover/movie?api_key=c41bd0194b74d8255c364fa70f725d1a&with_genres=28&language=es
+
+function renderPelicula(data) {
+  for (let i = 0; i < 12; i++) {
+    $("#peliculas").append(`<div id= "div-${data.results[i].id}"><h3>${data.results[i].title}</h3><img src="https://image.tmdb.org/t/p/w500${data.results[i].poster_path}"><button id= "movie-${data.results[i].id}"> Descripción</button></div>`);
+    $(`#movie-${data.results[i].id}`).click(cargarPelicula(data.results[i].id));
+  }
+}
+function detallesPelicula(data) {
+  $(`#div-${data.id}`).append(`<p>${data.overview}</p>`);
+
+}
+
+function cargarPelicula(id) {
+  $.ajax({
+    type: 'get',
+    dataType: 'json',
+    url: `https://api.themoviedb.org/3/movie/${id}?api_key=c41bd0194b74d8255c364fa70f725d1a&language=es`,
+    async: 'true',
+    success: function (data) { detallesPelicula(data) },
   });
 }
 
-function renderPelicula(data) {
-  $("#peliculas").append(`<div><h3>${data.results[0].title}</h3><img src="https://image.tmdb.org/t/p/w500${data.results[0].poster_path}"></div><h3>${data.results[0].overview}</h3>`);
-  $("#peliculas").append(`<div><h3>${data.results[1].title}</h3><img src="https://image.tmdb.org/t/p/w500${data.results[1].poster_path}"></div><h3>${data.results[1].overview}</h3>`);
-}
 
 $(document).ready(function () {
   cargarPeliculas();
