386416ade50b6f0a860adb77f9df0faca3ed1d5b
Micaela <94004537+micaela24c@users.noreply.github.com>
2021-12-26 11:04:08 -0500

    o

---
+++ assets/js/genero.js
@@ -0,0 +1,38 @@
+function cargarPeliculas() {
+    $.ajax({
+      type: 'get',
+      dataType: 'json',
+      url: `https://api.themoviedb.org/3/discover/movie?api_key=c41bd0194b74d8255c364fa70f725d1a&language=es`,
+      async: 'true',
+      success: function (data) { renderPelicula(data) },
+    });
+  }
+  //peliculas al azar de un género en específico: https://api.themoviedb.org/3/discover/movie?api_key=c41bd0194b74d8255c364fa70f725d1a&with_genres=28&language=es
+  // function(){cargarPelicula(data.results[i].id)}
+  
+  function renderPelicula(data) {
+    for (let i = 0; i < 12; i++) {
+      $("#peliculas").append(`<div id= "div-${data.results[i].id}"><h3>${data.results[i].title}</h3><img src="https://image.tmdb.org/t/p/w500${data.results[i].poster_path}"><button id= "movie-${data.results[i].id}"> Descripción</button></div>`);
+      $(`#movie-${data.results[i].id}`).click(function(){cargarPelicula(data.results[i].id)});
+    }
+  }
+  function detallesPelicula(data) {
+    $(`#div-${data.id}`).append(`<p>${data.overview}</p>`);
+  
+  }
+  
+  function cargarPelicula(id) {
+    $.ajax({
+      type: 'get',
+      dataType: 'json',
+      url: `https://api.themoviedb.org/3/movie/${id}?api_key=c41bd0194b74d8255c364fa70f725d1a&language=es`,
+      async: 'true',
+      success: function (data) { detallesPelicula(data) },
+    });
+  }
+  
+  
+  $(document).ready(function () {
+    cargarPeliculas();
+  })
+  
