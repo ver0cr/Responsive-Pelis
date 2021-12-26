7b97dfd394c54ef8521f7442bfb452aa78971513
Micaela <94004537+micaela24c@users.noreply.github.com>
2021-12-26 10:48:58 -0500

    i

--- assets/js/java.js
+++ assets/js/java.js
@@ -37,6 +37,24 @@ $(document).ready(function () {
 })
 
 
+function renderPic(data) {
+  // Manipulamos el DOM para agregar las imágenes
+  let div = $("<div></div>");
+  div.addClass("pic card");
+
+  let img = $("<img></img>");
+  img.attr("src", data.download_url);
+  img.addClass("card-img");
+  div.append(img);
+
+  let author = $("<h3></h3>");
+  author.addClass("card-title");
+  author.append(data.author);
+  div.append(author);
+
+  $('#pictures-container').append(div);
+}
+
 
 
 
