95171b4fcec87aa14a42765c02987ccd43ee487b
Micaela <94004537+micaela24c@users.noreply.github.com>
2021-12-28 10:54:27 -0500

    nue

--- assets/js/java.js
+++ assets/js/java.js
@@ -11,7 +11,7 @@ function cargarPeliculas() {
 // function(){cargarPelicula(data.results[i].id)}
 
 function renderPelicula(data) {
-  for (let i = 0; i < 12; i++) { 
+  for (let i = 0; i < data.results.length; i++) { 
     $("#peliculas").empty();
     $("#peliculas").append(`<div id= "div-${data.results[i].id}"><h3>${data.results[i].title}</h3><img src="https://image.tmdb.org/t/p/w500${data.results[i].poster_path}"><button id= "movie-${data.results[i].id}" class="btn-descripcion"> Descripción</button></div>`);
     $(`#movie-${data.results[i].id}`).click(function(){cargarPelicula(data.results[i].id)});
@@ -32,9 +32,22 @@ function cargarPelicula(id) {
   });
 }
 
+function buscarPelicula(texto) {
+  $.ajax({
+    type: 'get',
+    dataType: 'json',
+    url: `https://api.themoviedb.org/3/search/movie?api_key=c41bd0194b74d8255c364fa70f725d1a&language=es&query=${texto}`,
+    async: 'true',
+    success: function (data) { renderPelicula(data) },
+  });
+}
 
 $(document).ready(function () {
   cargarPeliculas();
+  $("#btn-busqueda").click(function(){
+    let texto=$("#texto-busqueda").val();
+    buscarPelicula(texto);
+    });
 })
 
 
