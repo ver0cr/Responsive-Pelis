1a7a57ee292120529ce3315b2e298cd93a787d30
Micaela <94004537+micaela24c@users.noreply.github.com>
2021-12-28 13:54:27 -0500

    w

--- assets/js/java.js
+++ assets/js/java.js
@@ -11,14 +11,15 @@ function cargarPeliculas() {
 // function(){cargarPelicula(data.results[i].id)}
 
 function renderPelicula(data) {
-  for (let i = 0; i < data.results.length; i++) { 
-    $("#peliculas").empty();
-    $("#peliculas").append(`<div id= "div-${data.results[i].id}"><h3>${data.results[i].title}</h3><img src="https://image.tmdb.org/t/p/w500${data.results[i].poster_path}"><button id= "movie-${data.results[i].id}" class="btn-descripcion"> Descripción</button></div>`);
-    $(`#movie-${data.results[i].id}`).click(function(){cargarPelicula(data.results[i].id)});
+  $("#peliculas").empty();
+  for (let i = 0; i < data.results.length; i++) {
+    $("#peliculas").append(`<div id="div-${data.results[i].id}"><h3>${data.results[i].title}</h3><img src="https://image.tmdb.org/t/p/w500${data.results[i].poster_path}"><button id="movie-${data.results[i].id}" class="btn-descripcion">Descripción</button></div>`);
+    $(`#movie-${data.results[i].id}`).click(function () { cargarPelicula(data.results[i].id) });
   }
 }
 function detallesPelicula(data) {
-  $(`#div-${data.id}`).append(`<p>${data.overview}</p>`);
+  $(`#detalles-${data.id}`).remove();
+  $(`#div-${data.id}`).append(`<p id="detalles-${data.id}">${data.overview}</p>`);
 
 }
 
@@ -38,20 +39,18 @@ function buscarPelicula(texto) {
     dataType: 'json',
     url: `https://api.themoviedb.org/3/search/movie?api_key=c41bd0194b74d8255c364fa70f725d1a&language=es&query=${texto}`,
     async: 'true',
-    success: function (data) { renderPelicula(data) },
+    success: function (data) { console.log(data); renderPelicula(data) },
+    error: function (data) {console.log("ERROR 404")}
   });
 }
 
 $(document).ready(function () {
   cargarPeliculas();
-  $("#btn-busqueda").click(function(){
-    let texto=$("#texto-busqueda").val();
+  $("#btn-busqueda").click(function () {
+    let texto = $("#texto-busqueda").val();
     buscarPelicula(texto);
-    });
+  });
 })
-
-
-
 
 
 
