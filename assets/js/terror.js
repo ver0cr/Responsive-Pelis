e04de647d95f3400daa09ff871e110995e88929a
Micaela <94004537+micaela24c@users.noreply.github.com>
2021-12-29 10:42:40 -0500

    q

--- assets/js/terror.js
+++ assets/js/terror.js
@@ -27,7 +27,7 @@ function cargarPeliculas() {
     $.ajax({
       type: 'get',
       dataType: 'json',
-      url: `https://api.themoviedb.org/3/discover/movie/${id}?api_key=c41bd0194b74d8255c364fa70f725d1a&with_genres=18&language=es`,
+      url: `https://api.themoviedb.org/3/discover/movie/${id}?api_key=c41bd0194b74d8255c364fa70f725d1a&with_genres=27&language=es`,
       async: 'true',
       success: function (data) { detallesPelicula(data) },
     });
@@ -37,7 +37,7 @@ function cargarPeliculas() {
     $.ajax({
       type: 'get',
       dataType: 'json',
-      url: `https://api.themoviedb.org/3/discover/movie?api_key=c41bd0194b74d8255c364fa70f725d1a&with_genres=18&language=es=${texto}`,
+      url: `https://api.themoviedb.org/3/discover/movie?api_key=c41bd0194b74d8255c364fa70f725d1a&with_genres=27&language=es=${texto}`,
       async: 'true',
       success: function (data) { console.log(data); renderPelicula(data) },
       error: function (data) {console.log("ERROR 404")}
